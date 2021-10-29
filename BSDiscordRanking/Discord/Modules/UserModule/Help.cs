5532a7051ec7427a360b9dd97553d69694519889
Jupilian <49618271+ASPJulien@users.noreply.github.com>
2021-10-29 19:52:25 +0200

    Help can now handle param for more specialised infomations

--- BSDiscordRanking/Discord/Modules/UserModule/Help.cs
+++ BSDiscordRanking/Discord/Modules/UserModule/Help.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using BSDiscordRanking.Controllers;
 using Discord;
@@ -13,41 +14,66 @@ namespace BSDiscordRanking.Discord.Modules.UserModule
     {
         [Command("help")]
         [Summary("Shows all the commands & their summaries.")]
-        public async Task Help()
+        public async Task Help(params string[] p_commands)
         {
-            bool l_IsAdmin = false;
-            if (Context.User is SocketGuildUser l_User)
-                if (l_User.Roles.Any(p_Role => p_Role.Id == ConfigController.GetConfig().BotManagementRoleID))
-                    l_IsAdmin = true;
-
-
-            int i = 0;
-            foreach (var l_Module in BotHandler.m_Commands.Modules)
+            if (p_commands == null)
             {
-                EmbedBuilder l_Builder = new EmbedBuilder();
-                l_Builder.WithTitle(l_Module.Name);
-                foreach (var l_Command in l_Module.Commands)
+                bool l_IsAdmin = false;
+                if (Context.User is SocketGuildUser l_User)
+                    if (l_User.Roles.Any(p_Role => p_Role.Id == ConfigController.GetConfig().BotManagementRoleID))
+                        l_IsAdmin = true;
+
+
+                int i = 0;
+                foreach (var l_Module in BotHandler.m_Commands.Modules)
                 {
-                    string l_Title = ConfigController.GetConfig().CommandPrefix.First() + l_Command.Name;
-                    foreach (var l_Parameter in l_Command.Parameters)
+                    EmbedBuilder l_Builder = new EmbedBuilder();
+                    l_Builder.WithTitle(l_Module.Name);
+                    foreach (var l_Command in l_Module.Commands)
+                    {
+                        string l_Title = ConfigController.GetConfig().CommandPrefix.First() + l_Command.Name;
+                        foreach (var l_Parameter in l_Command.Parameters)
+                        {
+                            l_Title += " [" + l_Parameter.Name.Replace("p_", "") + "]";
+                        }
+
+                        l_Builder.AddField(l_Title, l_Command.Summary, true);
+                        l_Builder.WithFooter("Prefix: " + Join(", ", ConfigController.GetConfig().CommandPrefix) + " | Bot made by Kuurama#3423 & Julien#1234");
+                    }
+
+                    if (l_Module.Name.Contains("Admin"))
+                    {
+                        if (!l_IsAdmin)
+                            continue;
+                        l_Builder.WithColor(Color.Red);
+                        l_Builder.Footer = null;
+                    }
+
+                    await Context.Channel.SendMessageAsync("", false, l_Builder.Build());
+                    i++;
+                }
+            }
+            else
+            {
+                string p_command = p_commands.First();
+                var l_FoundCommand = BotHandler.m_Commands.Commands.ToList().Find(x =>
+                    x.Name == p_command || x.Aliases.ToList().Find(x => x == p_command) == p_command);
+                if (l_FoundCommand != null)
+                {
+                    EmbedBuilder l_Builder = new EmbedBuilder();
+                    string l_Title = ConfigController.GetConfig().CommandPrefix.First() + l_FoundCommand.Name;
+                    foreach (var l_Parameter in l_FoundCommand.Parameters)
                     {
                         l_Title += " [" + l_Parameter.Name.Replace("p_", "") + "]";
                     }
 
-                    l_Builder.AddField(l_Title, l_Command.Summary, true);
-                    l_Builder.WithFooter("Prefix: " + Join(", ", ConfigController.GetConfig().CommandPrefix) + " | Bot made by Kuurama#3423 & Julien#1234");
+                    l_Builder.AddField(l_Title, l_FoundCommand.Summary, true);
+                    await Context.Channel.SendMessageAsync("", false, l_Builder.Build());
                 }
-
-                if (l_Module.Name.Contains("Admin"))
+                else
                 {
-                    if (!l_IsAdmin)
-                        continue;
-                    l_Builder.WithColor(Color.Red);
-                    l_Builder.Footer = null;
+                    await ReplyAsync("> :x: Sorry, command was not found.");
                 }
-
-                await Context.Channel.SendMessageAsync("", false, l_Builder.Build());
-                i++;
             }
         }
     }
