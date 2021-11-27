4f0f8ac553a490c33e8bb0911f0df2c2bc0d19e0
Kuurama <74458092+Kuurama@users.noreply.github.com>
2021-11-27 04:06:52 +0100

    Added !confirmationchannel for cheated and AdminPingOnPass difficulties

---
+++ BSDiscordRanking/Discord/Modules/AdminModule/ConfirmationChannel.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using BSDiscordRanking.Controllers;
+using Discord.Commands;
+
+namespace BSDiscordRanking.Discord.Modules.AdminModule
+{
+    [PermissionHandler.RequirePermissionAttribute(Permission)]
+    public partial class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("confirmationchannel")]
+        [Summary("Set this channel as the confirmation channel in order to be informed of cheated or AdminPingOnPass passed maps.")]
+        public async Task SetConfirmationChannel()
+        {
+            ConfigController.GetConfig();
+            ConfigController.m_ConfigFormat.AdminPingOnPassChannel = Context.Channel.Id;
+            ConfigController.ReWriteConfig();
+            await ReplyAsync("> :white_check_mark: This channel is now used as the ConfirmationChannel (PingOnPass and cheated score logging).");
+        }
+    }
+}
--- BSDiscordRanking/Discord/Modules/AdminModule/LoggingChannel.cs
+++ BSDiscordRanking/Discord/Modules/AdminModule/LoggingChannel.cs
@@ -9,13 +9,13 @@ namespace BSDiscordRanking.Discord.Modules.AdminModule
     {
         [Command("loggingchannel")]
         [Alias("logchannel")]
-        [Summary("Set this channel as a logs's message sending channel (Map Added/Deleted etc).")]
+        [Summary("Set this channel as the log messages sending channel (Map Added/Deleted/Edited etc).")]
         public async Task SetChannel()
         {
             ConfigController.GetConfig();
             ConfigController.m_ConfigFormat.LoggingChannel = Context.Channel.Id;
             ConfigController.ReWriteConfig();
-            await ReplyAsync("> :white_check_mark: This channel is now used as log-channel.");
+            await ReplyAsync("> :white_check_mark: This channel is now used as the log-channel.");
         }
     }
 }
